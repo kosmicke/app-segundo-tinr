32ad9d228111410a564a87370e9044a7798c4130
André Rodrigues <andre.urbanico@gmail.com>
2020-05-06 02:35:04 -0300

    fixes in observable

--- src/app/posts/posts-topics/posts-topics.component.ts
+++ src/app/posts/posts-topics/posts-topics.component.ts
@@ -1,32 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PostsTopicsService } from 'src/app/services/posts-topics.service';
-import { interval } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-posts-topics',
   templateUrl: './posts-topics.component.html',
   styleUrls: ['./posts-topics.component.scss']
 })
-export class PostsTopicsComponent implements OnInit {
+export class PostsTopicsComponent implements OnInit, OnDestroy {
 
   public topics: any[] = []
+  public subscription: Subscription
 
   constructor(
     private postsTopicsService: PostsTopicsService
   ) { }
 
-  ngOnInit() {
-
-    let novoObservable = interval(5000)
-
-    novoObservable.subscribe(times => {
-      this.loadTopics()
-    })
-
-  }
-
   async loadTopics(){
-
     try {
       let successRes = await this.postsTopicsService.getList()
       this.topics = successRes.data
@@ -35,8 +25,18 @@ export class PostsTopicsComponent implements OnInit {
     } catch (error) {
       console.log("errorRes", error)
     }
+  }
 
+  ngOnInit() {
+    this.loadTopics()
+    let novoObservable = interval(5000)
+    this.subscription = novoObservable.subscribe(times => {
+      this.loadTopics()
+    })
+  }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe()
   }
 
 }
